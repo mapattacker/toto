3f647327939ecf7e0aba1869a721999e4701b55e
Jake <teo.siyang@gmail.com>
2017-02-09 15:20:11 +0800

    Create toto_all_locations.py

---
+++ toto_all_locations.py
@@ -0,0 +1,23 @@
+# get all locations where one can buy TOTO
+import sqlite3, urllib
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# connect to database
+conn = sqlite3.connect('toto.sqlite')
+cur = conn.cursor()
+
+url='http://www.singaporepools.com.sg/outlets/Pages/lo_results.aspx?sppl=cz0mej1BJm89QSZjPUEmZD1B'
+html=urllib.urlopen(url).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+for a in soup.select('table[id="tblOutletSearchResult"] li'):
+    a= a.getText().replace('  ','').split('\n')
+    location = a[1].strip()
+    address= a[2].strip()
+    cur.execute("INSERT OR IGNORE INTO placeall(location,address) VALUES(?,?)",(location,address,))
+
+conn.commit()
+conn.close()
+print 'done'
